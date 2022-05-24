73b63915969cf6af16f13bc7f3a57732b1dd256f
Alexandre Prevot <a.prevot33@gmail.com>
2022-05-24 11:57:41 +0200

    reservations controller model and routes done

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,49 @@
+class ReservationsController < ApplicationController
+  before_action :set_reservation, only: %i[update show edit destroy]
+
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    if @reservation.save
+      redirect_to reservation_path(@reservation)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @reservation.update(reservation_params)
+      redirect_to reservation_path(@reservation)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+
+    if @reservation.destroy
+      redirect_to user_path(@reservation.user)
+    else
+      redirect_to user_reservation_path(@reservation)
+    end
+  end
+
+  private
+
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :end_date, :price, :total)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   resources :midgets
-  resources :user
+  resources :users do
+    resources :reservations, exept: [:delete]
+  end
+  delete 'reservation/:id', to: 'reservations#destroy'
 end
