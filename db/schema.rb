bae1a847aa580d83726ea4eb5d075737b076f34b
Cesar Victor <cesarvictor@hotmail.fr>
2022-05-24 12:02:31 +0200

    Add reference user to review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_24_092332) do
+ActiveRecord::Schema.define(version: 2022_05_24_100106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,9 @@ ActiveRecord::Schema.define(version: 2022_05_24_092332) do
     t.bigint "midget_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
     t.index ["midget_id"], name: "index_reviews_on_midget_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -49,4 +51,5 @@ ActiveRecord::Schema.define(version: 2022_05_24_092332) do
 
   add_foreign_key "midgets", "users"
   add_foreign_key "reviews", "midgets"
+  add_foreign_key "reviews", "users"
 end
